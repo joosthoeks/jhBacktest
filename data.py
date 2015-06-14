7b20859c67e717979b2bd2a61e7c2781af741037
joosthoeks <joosthoeks@gmail.com>
2015-06-14 19:47:13 +0000

    modified

--- data.py
+++ data.py
@@ -12,19 +12,23 @@ def getDataYahoo(dateStart, dateEnd, symbol='^AEX'):
 
     data = []
     for v in dataOrg:
-        data.append({'datetime': v['Date'],
+        data.append({
+            'datetime': v['Date'],
             'open': float(v['Open']),
             'high': float(v['High']),
             'low': float(v['Low']),
             'close': float(v['Close']),
 #            'close': float(v['Adj_Close']),
-            'volume': int(v['Volume'])})
+            'volume': int(v['Volume'])
+            })
 
-    data.append({'datetime': yahoo.get_trade_datetime(),
+    data.append({
+        'datetime': yahoo.get_trade_datetime(),
         'open': float(yahoo.get_open()),
         'high': float(yahoo.get_days_high()),
         'low': float(yahoo.get_days_low()),
         'close': float(yahoo.get_price()),
-        'volume': int(yahoo.get_volume())})
+        'volume': int(yahoo.get_volume())
+        })
 
     return data
