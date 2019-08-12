2a0e51ba111f5aabb4e693677c0f22ce542b6e25
joosthoeks <joosthoeks@gmail.com>
2019-08-12 15:47:31 +0000

    updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='jhBacktest',
-    version='20180111.0',
+    version='20190812.0',
     description='Backtest and analysis trading strategy with Python',
     keywords='backtest analysis trading strategy',
     url='https://github.com/joosthoeks/jhBacktest',
